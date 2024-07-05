0d2d1c2a4f4b77c73bc0f24bcccab4395d713693
Sebastian Balle <sbal@genmab.com>
2024-07-05 18:08:58 +0200

    Minor update

--- config.nix
+++ config.nix
@@ -1,16 +1,19 @@
-{
-  packageOverrides = pkgs: with pkgs; {
-    myPackages = pkgs.buildEnv {
-      name = "tools";
-      paths = [
-        stow
-        neovim
-        tmux
-        yazi
-        ripgrep
-        bat
-        fzf
-      ];
-    };
+{ pkgs ? import <nixpkgs> {} }:
+
+let
+  myPackages = pkgs.buildEnv {
+    name = "tools";
+    paths = [
+      pkgs.stow
+      pkgs.neovim
+      pkgs.tmux
+      pkgs.yazi
+      pkgs.ripgrep
+      pkgs.bat
+      pkgs.fzf
+    ];
   };
+in
+{
+  env = myPackages;
 }
